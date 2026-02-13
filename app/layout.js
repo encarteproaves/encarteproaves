387978931db6507b824a8937373f7ce9a708c42a
encarteproaves <romulojosepereirasilva@gmail.com>
2026-02-13 16:46:54 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,15 @@
+export const metadata = {
+  title: "Encarte Pro Aves",
+  description: "Aparelhos e caixas acústicas para encarte de canto."
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
 
