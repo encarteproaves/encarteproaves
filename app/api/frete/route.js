b978219d9a27bedf881d58a7950df98498c16b42
encarteproaves <romulojosepereirasilva@gmail.com>
2026-02-20 00:25:36 -0300

    Update route.js

--- app/api/frete/route.js
+++ app/api/frete/route.js
@@ -1,18 +1,16 @@
 export async function POST(req) {
-
   try {
-
     const body = await req.json();
 
-    const response = await fetch("https://melhorenvio.com.br/api/v2/me/shipment/calculate", {
+    const response = await fetch("https://sandbox.melhorenvio.com.br/api/v2/me/shipment/calculate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiOWVhNGQ1NWE5YTYzYTMwYjc3MjkzOTYwZjg4MzFhODZmZmE4OGIzNTBkMDVjMjQ1YmEyZWFlODljODg5MWYzMTAxZDc5OTQ2NDA5NmE0YzkiLCJpYXQiOjE3NzE1NTEzMjcuOTg1NjM5LCJuYmYiOjE3NzE1NTEzMjcuOTg1NjQxLCJleHAiOjE4MDMwODczMjcuOTY4Mjk0LCJzdWIiOiI2YjU1ZDBhNi0wNTg0LTQ5NWEtOWZkOS1lZWQ5ZTIwMmE4YzEiLCJzY29wZXMiOlsiY2FydC1yZWFkIiwiY2FydC13cml0ZSIsImNvbXBhbmllcy1yZWFkIiwiY29tcGFuaWVzLXdyaXRlIiwiY291cG9ucy1yZWFkIiwiY291cG9ucy13cml0ZSIsIm5vdGlmaWNhdGlvbnMtcmVhZCIsIm9yZGVycy1yZWFkIiwicHJvZHVjdHMtcmVhZCIsInByb2R1Y3RzLWRlc3Ryb3kiLCJwcm9kdWN0cy13cml0ZSIsInB1cmNoYXNlcy1yZWFkIiwic2hpcHBpbmctY2FsY3VsYXRlIiwic2hpcHBpbmctY2FuY2VsIiwic2hpcHBpbmctY2hlY2tvdXQiLCJzaGlwcGluZy1jb21wYW5pZXMiLCJzaGlwcGluZy1nZW5lcmF0ZSIsInNoaXBwaW5nLXByZXZpZXciLCJzaGlwcGluZy1wcmludCIsInNoaXBwaW5nLXNoYXJlIiwic2hpcHBpbmctdHJhY2tpbmciLCJlY29tbWVyY2Utc2hpcHBpbmciLCJ0cmFuc2FjdGlvbnMtcmVhZCIsInVzZXJzLXJlYWQiLCJ1c2Vycy13cml0ZSIsIndlYmhvb2tzLXJlYWQiLCJ3ZWJob29rcy13cml0ZSIsIndlYmhvb2tzLWRlbGV0ZSIsInRkZWFsZXItd2ViaG9vayJdfQ.IhOFWzqBOFyO16FA4M0g1_USms5u2lJKckK179FBAfcdM87tViqd2v_6GUb_tw3tI7X_GQrJG0CFenbLN5-nEZuoTbB2WzjtF-0w7OQRhs1ZNRePr7O-6fzlw_ep8jAkhcu2cJEyRTOD2CzsWRFEAMpM5-mPOALWvetfieU1rOhQ9M6YFZbQONPgAxdPDUI2jTupv1XTXsCyKPch43VTuICE6-dHVhKb96YwGg6t4EPouh82ypZZhWuyVRWwpgKMnpFnMMrWpYD_3vbNkjdmghX6gnxPEoeSIe_2dab-N6g7bm36PzpUsKzBSfANYqNN7xoeZgezOP0i9s9WgpZkUCLyjuKO3N1p9Mcy4U_OmIb5-KJFn19jc_vTuVPGYX84V8NEmntZdVKXZ1ppceNISOEjamBiV6AO7HJ3HGW4qIPykZ9rP-DjXWIvrvnXCboQMyRJm6RSFQwQvWYlf26taVY-5xbcdi4tZTzWqbVQT3cgV2pU-nKQC4MSLsX7W3HPFx_aUr4pI1alSNG34-d3WeqS4Nlyy8fjdVLHDB6UZZOw51jUfBRR6aTpk_e6rMidHhSLIJj87WUrcMiPO9ma4eEXpkTWqdT7GKyxco-mlo6812vbnKZXxH1qtC5vXOf1sR5ne0pU8Cc19NPluhh6mYGy-iIXC-VJo1p1PzhvIP0"
+        "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYWUyZWVlNzBiMDQ2YzA2YjRkZDc4ODg1YjM1NmNiMDQ3YjcxNWE5OWJkMjhmMjExZThlMjZlOTJjYmNhNmFhZDQ5NzIxNTdiNzcwODE4MjciLCJpYXQiOjE3NzE1NTc4NDMuOTQ3MjA0LCJuYmYiOjE3NzE1NTc4NDMuOTQ3MjA1LCJleHAiOjE4MDMwOTM4NDMuOTM1NDksInN1YiI6IjZiNTVkMGE2LTA1ODQtNDk1YS05ZmQ5LWVlZDllMjAyYThjMSIsInNjb3BlcyI6WyJjYXJ0LXJlYWQiLCJjYXJ0LXdyaXRlIiwiY29tcGFuaWVzLXJlYWQiLCJjb21wYW5pZXMtd3JpdGUiLCJjb3Vwb25zLXJlYWQiLCJjb3Vwb25zLXdyaXRlIiwibm90aWZpY2F0aW9ucy1yZWFkIiwib3JkZXJzLXJlYWQiLCJwcm9kdWN0cy1yZWFkIiwicHJvZHVjdHMtZGVzdHJveSIsInByb2R1Y3RzLXdyaXRlIiwicHVyY2hhc2VzLXJlYWQiLCJzaGlwcGluZy1jYWxjdWxhdGUiLCJzaGlwcGluZy1jYW5jZWwiLCJzaGlwcGluZy1jaGVja291dCIsInNoaXBwaW5nLWNvbXBhbmllcyIsInNoaXBwaW5nLWdlbmVyYXRlIiwic2hpcHBpbmctcHJldmlldyIsInNoaXBwaW5nLXByaW50Iiwic2hpcHBpbmctc2hhcmUiLCJzaGlwcGluZy10cmFja2luZyIsImVjb21tZXJjZS1zaGlwcGluZyIsInRyYW5zYWN0aW9ucy1yZWFkIiwidXNlcnMtcmVhZCIsInVzZXJzLXdyaXRlIiwid2ViaG9va3MtcmVhZCIsIndlYmhvb2tzLXdyaXRlIiwid2ViaG9va3MtZGVsZXRlIiwidGRlYWxlci13ZWJob29rIl19.FJYyJaq6xm3kCubvIgMEZN4MjIUBocC6tOkE4_7e3jt-4MwlKU3FmYj9i9gs_UmRYFdd06xtWUNzsL1On3tyZbnwxmAg4BAXe-QxiJrbrEQPDJCLzv1TurRlJYQqgRTWje_PVngJcmIr19F2wf3WxPHPfaZKHWEWoRMlL09Lauv0bZGvMeNsU7ZCvYQZW73JBZGjtyKXjUffbFe__MomXC2ez6639h_8-vaZINEFoT-1xG5wHtUaWsF7jY_CHEbZVvDvHM7IXZLKjNlTTxH02D_S4Jyfb-52x2ZKY9B2R4i6ScqooL8_g0phQsqjrl7sLcPhu3PWfCX4IQMnBm7xcAWge-pA18fCCDOv2UgcDyQ10ddhQnVbA0XTwlB8upzHfZXHJ2LJ_25PwhxWWKMzZcurYDhPMdGdTd0BSw07_KVYiLieYnislJdMre19gWenzpbvsxZ4bZaYhk9aH0SQyJBCV3mRcV3wdbuKYsVb5331rrW7tBHsRPBOUcHGRqioILJFPYebj8TNmZdlW_4axl_dGqbzLsNwrYzCT6H6E4tU24apFR2r8jIqw1Arv9fGrhtk9VKQ-BUA27yyNCloGDVz7zlnO4iHYYXqEmBTihuX8C1yxTE7W1rG7DBIj5zG19X4kvGAdiiPMJSw7LX0wdG4yYhs66ZuFFdyIk-i968"
       },
       body: JSON.stringify({
         from: {
-          postal_code: "08062670"
+          postal_code: "08062-670"
         },
         to: {
           postal_code: body.cep
@@ -20,12 +18,10 @@ export async function POST(req) {
         products: [
           {
             id: "1",
-            width: body.width,
-            height: body.height,
-            length: body.length,
-            weight: body.weight,
-            insurance_value: body.price,
-            quantity: 1
+            width: 20,
+            height: 20,
+            length: 20,
+            weight: 1
           }
         ]
       })
@@ -36,12 +32,6 @@ export async function POST(req) {
     return Response.json(data);
 
   } catch (error) {
-
-    return Response.json({
-      error: "Erro ao calcular frete"
-    });
-
+    return Response.json({ error: "Erro no cálculo" });
   }
-
 }
-
