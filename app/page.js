87649a6405b3893316888c22ecfc74cf34b9915e
encarteproaves <romulojosepereirasilva@gmail.com>
2026-02-19 23:47:22 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
       id: 1,
       name: "Caixa Acústica Profissional para encarte de canto em aves",
       description: "Caixa acústica profissional para encarte de canto em pássaros, - Medidas 65x65x35 cm auto falante de auto rendimento, perfeita para um bom aprendizado de canto",
-      price: 1.500,
+      price: 1500,
       image: "/caixa-nova.png",
       weight: 8,
       width: 65,
