9e8208297e020ddac3ad1da90d4ed35618d91c0b
encarteproaves <romulojosepereirasilva@gmail.com>
2026-02-25 13:42:25 -0300

    Update route.js

--- app/api/pedido/route.js
+++ app/api/pedido/route.js
@@ -1,1 +1,24 @@
+let pedidos = [];
 
+export async function POST(req){
+  const body = await req.json();
+
+  const novoPedido = {
+    id: Date.now(),
+    produto: body.produto,
+    valor: body.valor,
+    cep: body.cep,
+    frete: body.frete,
+    canto: body.canto || "",
+    status: "Aguardando etiqueta",
+    data: new Date()
+  };
+
+  pedidos.push(novoPedido);
+
+  return Response.json(novoPedido);
+}
+
+export async function GET(){
+  return Response.json(pedidos);
+}
