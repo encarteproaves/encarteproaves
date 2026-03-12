a19ddf1196a42493f43179458295d8ad6b8766f4
encarteproaves <romulojosepereirasilva@gmail.com>
2026-03-12 20:44:33 -0300

    Update route.js

--- app/api/mercadopago/webhook/route.js
+++ app/api/mercadopago/webhook/route.js
@@ -1,9 +1,45 @@
 export async function POST(req){
-  const token = process.env.MERCADOPAGO_TOKEN
-  const body = await req.json()
 
-  console.log("Webhook recebido:", body)
+  try{
 
-  return Response.json({ok:true})
+    const token = process.env.MERCADOPAGO_TOKEN
+
+    const body = await req.json()
+
+    console.log("Webhook recebido:", body)
+
+    const paymentId = body.data?.id
+
+    if(paymentId){
+
+      const pagamento = await fetch(
+        `https://api.mercadopago.com/v1/payments/${paymentId}`,
+        {
+          headers:{
+            Authorization:`Bearer ${token}`
+          }
+        }
+      )
+
+      const data = await pagamento.json()
+
+      console.log("Status do pagamento:", data.status)
+
+      if(data.status === "approved"){
+        console.log("Pagamento aprovado")
+      }
+
+    }
+
+    return Response.json({ok:true})
+
+  }catch(err){
+
+    console.log(err)
+
+    return Response.json({erro:true})
+
+  }
 
 }
+
