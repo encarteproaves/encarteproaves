28568353e5459e35ab1b24805036011042acbe87
encarteproaves <romulojosepereirasilva@gmail.com>
2026-03-18 13:31:09 -0300

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,8 +1,15 @@
 export default function AdminPage() {
   return (
     <div>
-      <h1>Painel Administrativo</h1>
-      <p>Bem-vindo ao painel</p>
+      <h1>Dashboard</h1>
+
+      <p>Resumo do sistema:</p>
+
+      <ul>
+        <li>Total de pedidos</li>
+        <li>Pedidos enviados</li>
+        <li>Pedidos entregues</li>
+      </ul>
     </div>
   );
 }
