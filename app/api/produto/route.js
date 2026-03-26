ab05f6a6aa111fbae74fea71dad9975632a1c4f1
encarteproaves <romulojosepereirasilva@gmail.com>
2026-03-26 12:25:50 -0300

    fix api produto funcional

--- app/api/produto/route.js
+++ app/api/produto/route.js
@@ -1,9 +1,39 @@
+let produtos = [
+  {
+    id: 1,
+    nome: "Caixa Acústica Profissional",
+    preco: 1500,
+    descricao: "Caixa acústica para encarte de canto",
+  },
+  {
+    id: 2,
+    nome: "Aparelho Digital",
+    preco: 330,
+    descricao: "Programador automático de canto",
+  },
+  {
+    id: 3,
+    nome: "Pen Drive 8GB",
+    preco: 150,
+    descricao: "Canto personalizado",
+  },
+];
+
 export async function GET() {
-  return Response.json([
-    {
-      id: 1,
-      nome: "Produto teste",
-      preco: 100
-    }
-  ]);
+  return Response.json(produtos);
+}
+
+export async function POST(req) {
+  const body = await req.json();
+
+  const novoProduto = {
+    id: Date.now(),
+    nome: body.nome,
+    preco: Number(body.preco),
+    descricao: body.descricao,
+  };
+
+  produtos.push(novoProduto);
+
+  return Response.json(novoProduto);
 }
