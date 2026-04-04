8500bf0ca5c82385783c8b31a2bd27f19af254b7
encarteproaves <romulojosepereirasilva@gmail.com>
2026-04-04 14:21:06 -0300

    fix runtime nodejs vercel

--- app/api/admin/produto/route.js
+++ app/api/admin/produto/route.js
@@ -1,5 +1,5 @@
 export const dynamic = "force-dynamic";
-
+export const runtime = "nodejs";
 import { prisma } from '../../../../lib/prisma'
 
 // ✅ GET (listar produtos)
--- app/api/public/produto/route.js
+++ app/api/public/produto/route.js
@@ -1,4 +1,5 @@
 export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
